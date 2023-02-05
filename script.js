39ffa1045af25f5fa58c10b0bcd20b433243260e
Jasper Mayone <jasper@jaspermayone.com>
2023-02-05 18:01:04 -0500

    :construction: more wip script work

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
 const axios = require('axios');
 const fs = require('fs');
-require('dotenv').config()
+require('dotenv').config();
 
-const key = process.env.API_KEY
+const key = process.env.API_KEY;
 
 console.log('Starting script...');
 
-// Read the list of codes from the JSON file
+// Read the codes.json file. Each object has an id and a term.
 console.log('Reading codes from codes.json...');
-fs.readFile('codes.json', 'utf8', (err, data) => {
+fs.readFile('codes.json', 'utf8', async (err, data) => {
     if (err) {
         console.error(err);
         return;
     }
 
-    // Parse the JSON data
+    // Parse the JSON data into an array of objects.
     const codes = JSON.parse(data);
 
+    // Array to hold the responses.
     const responseData = [];
 
-    // Loop through the codes list
-    codes.forEach(async (code, index) => {
+    // Array to hold promises.
+    const promises = [];
 
-        try {
-            //  console.log('Making requests to UTS API...');
-
-            // Make the GET request
-            const response1 = await axios.get(`https://uts-ws.nlm.nih.gov/rest/search/current/?string=${code}&apiKey=${key}`);
-
-
-            const results = response1.data.result.results.map(result => {
-                return { name: result.name, uri: result.uri };
-            });
-
-            // Add the results array from the response data to the responseData array
-            responseData[index] = {
-                code: code,
-                results: results
-            };
-
-            // Check if all the requests have completed
-            if (responseData.length === codes.length) {
-                // Write the response data to a JSON file
-                // console.log('Writing response data to response_data.json...');
-                fs.writeFile('response_data.json', JSON.stringify(responseData), 'utf8', err => {
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        //console.log('Response data written to response_data.json');
-                    }
-                });
-            }
-        } catch (error) {
-            // Print the error message
-            console.error(`Failed to retrieve data for code ${code}: ${error}`);
-        }
+    // For each code, make a request to the API using the code's term and add the promise to the array.
+    console.log('Making requests to API...');
+    codes.forEach(code => {
+        const url = `https://uts-ws.nlm.nih.gov/rest/search/current/?string=${code.term}&apiKey=${key}&searchType=exact`;
+        promises.push(axios.get(url));
     });
 
+    // Wait for all promises to resolve.
+    await Promise.all(promises)
+        .then(responses => {
+            // For each response, extract the desired information and add it to the responseData array.
+            responses.forEach((response, index) => {
+                const result = {
+                    id: codes[index].id,
+                    term: response.data.result.results[0].name,
+                    uri: response.data.result.results[0].uri
+                };
+                responseData.push(result);
+            });
+        })
+        .catch(error => {
+            console.error(`Failed to retrieve data for code: ${error}`);
+        });
+
+    // Write the response data to a file.
+    console.log('Writing response data to file...');
+    fs.writeFile('response.json', JSON.stringify(responseData), err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('File written successfully.');
+    });
 });
+
 console.log('Script complete.');
